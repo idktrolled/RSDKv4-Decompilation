d327f8254fe0f6d123674103586cf45b1665faf9
idktrolled <camilocarlos1100@gmail.com>
2025-03-10 23:27:39 -0500

    Add files via upload

--- Android.mk
+++ Android.mk
@@ -3,6 +3,9 @@ LOCAL_PATH := $(call my-dir)
 OGG_DIR := dependencies/android/libogg
 VORBIS_DIR := dependencies/android/libvorbis
 THEORA_DIR := dependencies/android/libtheora
+TXML_DIR := dependencies/all/tinyxml2
+ASIO_DIR := dependencies/all/asio/asio/include
+STBIMG_DIR := dependencies/all/stb-image
 
 OGG_INCLUDES    := $(LOCAL_PATH)/$(OGG_DIR)/include
 VORBIS_INCLUDES := $(LOCAL_PATH)/$(VORBIS_DIR)/include \
@@ -78,28 +81,33 @@ LOCAL_MODULE := main
 SDL_PATH := ../SDL
 
 LOCAL_CFLAGS   := -fexceptions
-LOCAL_CPPFLAGS :=  -std=c++17
-APP_CPPFLAGS :=  -std=c++17
 
 LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/RSDKv4 \
+    $(LOCAL_PATH)/RSDKv4/NativeObjects \
     $(LOCAL_PATH)/$(SDL_PATH)/include \
     $(LOCAL_PATH)/dependencies/all \
     $(LOCAL_PATH)/$(VORBIS_DIR)/include \
     $(LOCAL_PATH)/$(OGG_DIR)/include \
     $(LOCAL_PATH)/$(THEORA_DIR)/include \
     $(LOCAL_PATH)/dependencies/all/theoraplay \
-    $(LOCAL_PATH)/dependencies/all/tinyxml2 \
+    $(LOCAL_PATH)/$(TXML_DIR) \
+    $(LOCAL_PATH)/$(STBIMG_DIR) \
+    $(LOCAL_PATH)/$(ASIO_DIR)
 
 
 WILDCARD_SETUP := \
+  $(wildcard $(LOCAL_PATH)/RSDKv4/*.cpp) \
+  $(LOCAL_PATH)/RSDKv4/NativeObjects/All.cpp \
   $(wildcard $(LOCAL_PATH)/dependencies/all/theoraplay/*.c) \
-  $(wildcard $(LOCAL_PATH)/dependencies/all/tinyxml2/*.cpp) \
-  $(wildcard $(LOCAL_PATH)/RSDKv3/*.cpp)
+  $(wildcard $(LOCAL_PATH)/dependencies/all/stb-image/*.cpp) \
+  $(wildcard $(LOCAL_PATH)/dependencies/all/tinyxml2/*.cpp)
 
 LOCAL_SRC_FILES := $(subst jni/src/, , $(WILDCARD_SETUP))
 
-LOCAL_LDLIBS := -lGLESv1_CM -llog
-
 LOCAL_SHARED_LIBRARIES := SDL2 libogg libvorbis libtheora
+
+
+LOCAL_LDLIBS := -lGLESv1_CM -llog
 
 include $(BUILD_SHARED_LIBRARY)
