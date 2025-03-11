ba763d4d82503f9ac63a148c2b763d78c6e4563f
idktrolled <camilocarlos1100@gmail.com>
2025-03-10 23:26:43 -0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,14 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 OGG_DIR := dependencies/android/libogg
 VORBIS_DIR := dependencies/android/libvorbis
-OGG_DIR := dependencies/android/libogg
-TXML_DIR := dependencies/all/tinyxml2
-ASIO_DIR := dependencies/all/asio/asio/include
-STBIMG_DIR := dependencies/all/stb-image
+THEORA_DIR := dependencies/android/libtheora
 
 OGG_INCLUDES    := $(LOCAL_PATH)/$(OGG_DIR)/include
 VORBIS_INCLUDES := $(LOCAL_PATH)/$(VORBIS_DIR)/include \
 	                 $(LOCAL_PATH)/$(VORBIS_DIR)/lib
+THEORA_INCLUDES := $(LOCAL_PATH)/$(THEORA_DIR)/include \
+	                 $(LOCAL_PATH)/$(THEORA_DIR)/lib
 
 ######################################################################
 # OGG
@@ -52,6 +51,25 @@ LOCAL_SRC_FILES := \
 include $(BUILD_STATIC_LIBRARY)
 
 ######################################################################
+# THEORA
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+LOCAL_MODULE   := libtheora
+LOCAL_CFLAGS   := -ffast-math -fsigned-char -O2 -fPIC -DPIC \
+                  -DBYTE_ORDER=LITTLE_ENDIAN -D_ARM_ASSEM_
+
+LOCAL_C_INCLUDES := $(OGG_INCLUDES) $(THEORA_INCLUDES)
+
+WILDCARD_SETUP := \
+  $(wildcard $(LOCAL_PATH)/$(THEORA_DIR)/lib/*.c)
+
+LOCAL_SRC_FILES := \
+	$(subst jni/src/, , $(WILDCARD_SETUP))
+
+include $(BUILD_STATIC_LIBRARY)
+
+######################################################################
 
 include $(CLEAR_VARS)
 
@@ -60,29 +78,28 @@ LOCAL_MODULE := main
 SDL_PATH := ../SDL
 
 LOCAL_CFLAGS   := -fexceptions
+LOCAL_CPPFLAGS :=  -std=c++17
+APP_CPPFLAGS :=  -std=c++17
 
 LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/RSDKv4 \
-    $(LOCAL_PATH)/RSDKv4/NativeObjects \
     $(LOCAL_PATH)/$(SDL_PATH)/include \
     $(LOCAL_PATH)/dependencies/all \
     $(LOCAL_PATH)/$(VORBIS_DIR)/include \
     $(LOCAL_PATH)/$(OGG_DIR)/include \
-    $(LOCAL_PATH)/$(TXML_DIR) \
-    $(LOCAL_PATH)/$(STBIMG_DIR) \
-    $(LOCAL_PATH)/$(ASIO_DIR)
+    $(LOCAL_PATH)/$(THEORA_DIR)/include \
+    $(LOCAL_PATH)/dependencies/all/theoraplay \
+    $(LOCAL_PATH)/dependencies/all/tinyxml2 \
 
 
 WILDCARD_SETUP := \
-  $(wildcard $(LOCAL_PATH)/RSDKv4/*.cpp) \
-  $(LOCAL_PATH)/RSDKv4/NativeObjects/All.cpp \
-  $(wildcard $(LOCAL_PATH)/dependencies/all/stb-image/*.cpp) \
-  $(wildcard $(LOCAL_PATH)/dependencies/all/tinyxml2/*.cpp)
+  $(wildcard $(LOCAL_PATH)/dependencies/all/theoraplay/*.c) \
+  $(wildcard $(LOCAL_PATH)/dependencies/all/tinyxml2/*.cpp) \
+  $(wildcard $(LOCAL_PATH)/RSDKv3/*.cpp)
 
 LOCAL_SRC_FILES := $(subst jni/src/, , $(WILDCARD_SETUP))
 
-LOCAL_SHARED_LIBRARIES := SDL2 libogg libvorbis
-
 LOCAL_LDLIBS := -lGLESv1_CM -llog
+
+LOCAL_SHARED_LIBRARIES := SDL2 libogg libvorbis libtheora
 
 include $(BUILD_SHARED_LIBRARY)
