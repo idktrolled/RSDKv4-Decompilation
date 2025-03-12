fb05236da8c54fca9e82752206931b85eddb08dc
idktrolled <camilocarlos1100@gmail.com>
2025-03-11 23:14:10 -0500

    Add files via upload

--- RSDKv4/theoraplay.h
+++ RSDKv4/theoraplay.h
@@ -23,6 +23,15 @@ struct THEORAPLAY_Io
     void *userdata;
 };
 
+typedef struct THEORAPLAY_Allocator THEORAPLAY_Allocator;
+struct THEORAPLAY_Allocator
+{
+    void *(*allocate)(const THEORAPLAY_Allocator *allocator, unsigned int len);
+    void (*deallocate)(const THEORAPLAY_Allocator *allocator, void *ptr);
+    void *userdata;
+};
+
+
 typedef struct THEORAPLAY_Decoder THEORAPLAY_Decoder;
 
 /* YV12 is YCrCb, not YCbCr; that's what SDL uses for YV12 overlays. */
@@ -31,7 +40,9 @@ typedef enum THEORAPLAY_VideoFormat
     THEORAPLAY_VIDFMT_YV12,  /* NTSC colorspace, planar YCrCb 4:2:0 */
     THEORAPLAY_VIDFMT_IYUV,  /* NTSC colorspace, planar YCbCr 4:2:0 */
     THEORAPLAY_VIDFMT_RGB,   /* 24 bits packed pixel RGB */
-    THEORAPLAY_VIDFMT_RGBA   /* 32 bits packed pixel RGBA (full alpha). */
+    THEORAPLAY_VIDFMT_RGBA,  /* 32 bits packed pixel RGBA (full alpha). */
+    THEORAPLAY_VIDFMT_BGRA,  /* 32 bits packed pixel BGRA (full alpha). */
+    THEORAPLAY_VIDFMT_RGB565 /* 16 bits packed pixel RGB565. */
 } THEORAPLAY_VideoFormat;
 
 typedef struct THEORAPLAY_VideoFrame
@@ -59,11 +70,18 @@ typedef struct THEORAPLAY_AudioPacket
 
 THEORAPLAY_Decoder *THEORAPLAY_startDecodeFile(const char *fname,
                                                const unsigned int maxframes,
-                                               THEORAPLAY_VideoFormat vidfmt);
+                                               THEORAPLAY_VideoFormat vidfmt,
+                                               const THEORAPLAY_Allocator *allocator,
+                                               const int multithreaded);
 THEORAPLAY_Decoder *THEORAPLAY_startDecode(THEORAPLAY_Io *io,
                                            const unsigned int maxframes,
-                                           THEORAPLAY_VideoFormat vidfmt);
+                                           THEORAPLAY_VideoFormat vidfmt,
+                                           const THEORAPLAY_Allocator *allocator,
+                                           const int multithreaded);
 void THEORAPLAY_stopDecode(THEORAPLAY_Decoder *decoder);
+
+// call this frequently if not multithreaded! Safe no-op if multithreaded.
+void THEORAPLAY_pumpDecode(THEORAPLAY_Decoder *decoder, const int maxframes);
 
 int THEORAPLAY_isDecoding(THEORAPLAY_Decoder *decoder);
 int THEORAPLAY_decodingError(THEORAPLAY_Decoder *decoder);
